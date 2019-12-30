3d2de184c301a095df1045814c5993d5d21b110a
Radames-prog <easycompvila@gmail.com>
2019-12-30 16:46:25 -0300

    Updates

---
+++ precache-manifest.a45380cf94ca2eac1ec1fb49464a8edd.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "86ffbc61349e6a3c945cf935a5857472",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a589f21b9c76bd9a9075",
+    "url": "/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "fa6b3d3b28000b3a2886",
+    "url": "/static/css/main.2e289d3a.chunk.css"
+  },
+  {
+    "revision": "a589f21b9c76bd9a9075",
+    "url": "/static/js/2.c7162b5e.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.c7162b5e.chunk.js.LICENSE"
+  },
+  {
+    "revision": "fa6b3d3b28000b3a2886",
+    "url": "/static/js/main.1a2e056d.chunk.js"
+  },
+  {
+    "revision": "cf9e28ca9ace9a259057",
+    "url": "/static/js/runtime-main.3a9db239.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
